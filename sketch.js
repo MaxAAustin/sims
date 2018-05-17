f8ff1a5605d6ddd37a11eaf150469d754dc6bdec
michael_austin <maxaustin92@gmail.com>
2018-05-17 14:45:02 -0400

    Comments added to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
-
+/*
+* attractors are essentially the planets, or entities that draw the particles in. Stored as an array.
+* particles are instances of the Particle class that are pulled in to the attractor objects based on their Euclidean distance from the attractors and attractor mass (area of the circles).
+*/
 var attractors = [];
 var particles = [];
 
