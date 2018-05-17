2feeea3688357df6161669b29e1fd367e6201276
michael_austin <maxaustin92@gmail.com>
2018-05-17 14:08:49 -0400

    Added to sketch.js and index.

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+var attractors = [];
+var particles = [];
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function mousePressed() {
+    attractors.push(createVector(mouseX, mouseY));
+}
+
+function draw() {
+
+}
