86054fecd531ae21484b28db2552b188cc7f0cdc
MaxAAustin <maxaustin92@gmail.com>
2018-06-15 16:32:55 -0400

    Added three.js to libs

--- js/planetSketch.js
+++ js/planetSketch.js
@@ -1,22 +1,6 @@
 /*
 * Author: Michael Austin
 * Date: 06/15/2018
-* External Libs: p5.js and WebGL from MDN
+* External API: WebGL from MDN
 *Sets up planet canvas and draws 3D planet model with other attributes like background, color, etc.
 */
-var earth;
-
-function preload() {
-    earth = loadImage('img/venusmap.jpg');
-}
-
-function setup() {
-    createCanvas(500, 500, WEBGL);
-}
-function draw() {
-    background(0);
-    //ambientLight(0,0,255);
-    texture(earth);
-    //ambientMaterial(255);
-    box(200, 200, 200);
-}
