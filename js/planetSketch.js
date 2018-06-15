e282170022d08a7ad2a4ffa084d15595d979cdd0
MaxAAustin <maxaustin92@gmail.com>
2018-06-15 04:30:20 -0400

    messing with planetSketch

--- js/planetSketch.js
+++ js/planetSketch.js
@@ -4,18 +4,19 @@
 * External Libs: p5.js and WebGL from MDN
 *Sets up planet canvas and draws 3D planet model with other attributes like background, color, etc.
 */
-
-let img;
+var earth;
 
 function preload() {
-    img = loadImage('textures/Earth.png');
+    earth = loadImage('img/venusmap.jpg');
 }
+
 function setup() {
     createCanvas(500, 500, WEBGL);
 }
 function draw() {
-    background(175);
-
-    texture(img);
-    plane(100,100);
+    background(0);
+    //ambientLight(0,0,255);
+    texture(earth);
+    //ambientMaterial(255);
+    box(200, 200, 200);
 }
