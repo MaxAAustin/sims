d387868be9e4517d26a2f5f713f178759423b272
MaxAAustin <maxaustin92@gmail.com>
2018-06-17 21:47:14 -0400

    Leaned down the libs folder

--- js/planetSketch.js
+++
@@ -1,6 +0,0 @@
-/*
-* Author: Michael Austin
-* Date: 06/15/2018
-* External API: WebGL from MDN
-*Sets up planet canvas and draws 3D planet model with other attributes like background, color, etc.
-*/
